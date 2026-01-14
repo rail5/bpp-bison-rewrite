d11d41226a8ea38f9be62889de59aa3aba57ed91
rail5 <andrew@rail5.org>
2026-01-15 00:53:15 +0800

    Cleaner algorithm for ASTNode::addChildren

--- src/AST/ASTNode.cpp
+++ src/AST/ASTNode.cpp
@@ -41,41 +41,29 @@ void ASTNode::addChild(const std::shared_ptr<ASTNode>& child) {
 void ASTNode::addChildren(const std::vector<std::shared_ptr<ASTNode>>& childs) {
 	if (childs.empty()) return;
 
-	std::vector<std::shared_ptr<ASTNode>> filtered_children;
-	filtered_children.reserve(childs.size());
+	children.reserve(children.size() + childs.size());
+
+	auto lastRawText = std::dynamic_pointer_cast<AST::RawText>(children.empty() ? nullptr : children.back());
 
 	for (const auto& child : childs) {
 		if (child == nullptr) continue;
 
-		if (child->getType() == AST::NodeType::RawText
-			&& !filtered_children.empty()
-			&& filtered_children.back()->getType() == AST::NodeType::RawText
-		) {
+		if (child->getType() != AST::NodeType::RawText) {
+			children.push_back(child);
+			lastRawText = nullptr;
+			continue;
+		}
+
+		// Child is RawText
+		if (lastRawText != nullptr) {
 			// Merge with last RawText child
-			auto lastRawText = std::dynamic_pointer_cast<AST::RawText>(filtered_children.back());
 			auto newRawText = std::dynamic_pointer_cast<AST::RawText>(child);
 			lastRawText->appendText(newRawText->TEXT());
-			continue;
 		} else {
-			filtered_children.push_back(child);
+			children.push_back(child);
+			lastRawText = std::dynamic_pointer_cast<AST::RawText>(child);
 		}
 	}
-
-	if (filtered_children.empty()) return;
-
-	if (filtered_children.front()->getType() == AST::NodeType::RawText
-		&& !children.empty()
-		&& children.back()->getType() == AST::NodeType::RawText
-	) {
-		// Merge first RawText child with last existing RawText child
-		auto lastRawText = std::dynamic_pointer_cast<AST::RawText>(children.back());
-		auto newRawText = std::dynamic_pointer_cast<AST::RawText>(filtered_children.front());
-		lastRawText->appendText(newRawText->TEXT());
-		filtered_children.erase(filtered_children.begin());
-		if (filtered_children.empty()) return;
-	}
-
-	children.insert(children.end(), filtered_children.begin(), filtered_children.end());
 }
 
 const std::vector<std::shared_ptr<ASTNode>>& ASTNode::getChildren() const {
