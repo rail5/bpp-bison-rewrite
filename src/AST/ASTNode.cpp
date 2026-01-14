a4ea7655c3f67eb64fef791c81a0cfb6ee4f2825
rail5 <andrew@rail5.org>
2026-01-14 13:37:27 +0800

    ASTNode: Always merge incoming consecutive RawText nodes, always reject nullptr children

--- src/AST/ASTNode.cpp
+++ src/AST/ASTNode.cpp
@@ -7,6 +7,14 @@ AST::NodeType ASTNode::getType() const {
 	return type;
 }
 
+/**
+ * @brief Add a child node to this AST node.
+ * This function also:
+ *  1. Filters out null child nodes
+ *  2. Merges consecutive RawText nodes into a single RawText node to optimize the AST structure.
+ * 
+ * @param child The child AST node to add.
+ */
 void ASTNode::addChild(const std::shared_ptr<ASTNode>& child) {
 	if (child == nullptr) return;
 	if (child->getType() == AST::NodeType::RawText
@@ -22,8 +30,52 @@ void ASTNode::addChild(const std::shared_ptr<ASTNode>& child) {
 	children.push_back(child);
 }
 
+/**
+ * @brief Add a vector of child nodes to this AST node.
+ * This function also:
+ *  1. Filters out null child nodes
+ *  2. Merges consecutive RawText nodes into a single RawText node to optimize the AST structure.
+ * 
+ * @param childs The vector of child AST nodes to add.
+ */
 void ASTNode::addChildren(const std::vector<std::shared_ptr<ASTNode>>& childs) {
-	children.insert(children.end(), childs.begin(), childs.end());
+	if (childs.empty()) return;
+
+	std::vector<std::shared_ptr<ASTNode>> filtered_children;
+	filtered_children.reserve(childs.size());
+
+	for (const auto& child : childs) {
+		if (child == nullptr) continue;
+
+		if (child->getType() == AST::NodeType::RawText
+			&& !filtered_children.empty()
+			&& filtered_children.back()->getType() == AST::NodeType::RawText
+		) {
+			// Merge with last RawText child
+			auto lastRawText = std::dynamic_pointer_cast<AST::RawText>(filtered_children.back());
+			auto newRawText = std::dynamic_pointer_cast<AST::RawText>(child);
+			lastRawText->appendText(newRawText->TEXT());
+			continue;
+		} else {
+			filtered_children.push_back(child);
+		}
+	}
+
+	if (filtered_children.empty()) return;
+
+	if (filtered_children.front()->getType() == AST::NodeType::RawText
+		&& !children.empty()
+		&& children.back()->getType() == AST::NodeType::RawText
+	) {
+		// Merge first RawText child with last existing RawText child
+		auto lastRawText = std::dynamic_pointer_cast<AST::RawText>(children.back());
+		auto newRawText = std::dynamic_pointer_cast<AST::RawText>(filtered_children.front());
+		lastRawText->appendText(newRawText->TEXT());
+		filtered_children.erase(filtered_children.begin());
+		if (filtered_children.empty()) return;
+	}
+
+	children.insert(children.end(), filtered_children.begin(), filtered_children.end());
 }
 
 const std::vector<std::shared_ptr<ASTNode>>& ASTNode::getChildren() const {
